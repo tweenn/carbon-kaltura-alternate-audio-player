aab20776b4175de791118cc307225fd9a1404023
Felipe Zuntini <felipezuntini@hotmail.com>
2022-11-01 09:14:52 -0300

    44c0119

---
+++ main.0bfca4a96a7d04ded9b2.manager.bundle.js
@@ -0,0 +1,13 @@
+/**
+ * @license MIT
+ * @description
+ * Carbon Kaltura Alternate Audio Player:
+ * This is a community component, provided as is
+ * Environment: Production
+ * Built: Nov 1, 2022, 9:14:38 AM
+ * @version 1.1.0
+ * @author Felipe Zuntini <felipezuntini@hotmail.com>
+ * @example https://tweenn.github.io/carbon-kaltura-alternate-audio-player/
+ * @url https://github.com/tweenn/carbon-kaltura-alternate-audio-player
+ */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{445:function(module,exports,__webpack_require__){__webpack_require__(446),__webpack_require__(823),__webpack_require__(827),__webpack_require__(828),__webpack_require__(824),__webpack_require__(818),__webpack_require__(821),__webpack_require__(822),__webpack_require__(820),__webpack_require__(825),__webpack_require__(826),module.exports=__webpack_require__(819)},492:function(module,exports){}},[[445,3,4]]]);
